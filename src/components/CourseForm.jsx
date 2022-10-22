fe4faa078935e6a29193dfeb363086f265dee248
Shashanka8 <nayak.shashanka97@gmail.com>
2022-10-22 15:16:35 +0530

    Added a form to add course and able to store data in courseStore also in localstorage

---
+++ src/components/CourseForm.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+import useCourseStore from "../app/courseStore";
+
+const CourseForm = () => {
+  const addCourse = useCourseStore((state) => state.addCourse);
+
+  const [courseTitle, setCourseTitle] = useState("");
+
+  const handleCourseSubmit = () => {
+    if (!courseTitle) return alert("please enter a course title");
+
+    addCourse({
+      id: Math.ceil(Math.random() * 1000000),
+      tilte: courseTitle,
+    });
+  };
+
+  return (
+    <div className="form-container">
+      <input
+        value={courseTitle}
+        onChange={(e) => setCourseTitle(e.target.value)}
+        className="form-input"
+      />
+      <button onClick={handleCourseSubmit} className="form-submit-btn">
+        Add Course
+      </button>
+    </div>
+  );
+};
+
+export default CourseForm;
